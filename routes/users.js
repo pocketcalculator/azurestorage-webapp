a10049c3d8f7df10ee6b0c52007fe73fa15d05e1
Paul Sczurek <pocketcalculatorshow@gmail.com>
2023-09-26 00:23:55 -0400

    clean dir structure

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,17 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { getUser, getUsers, createUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age:  22,
-        id: 123
-    }
-];
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-    res.send(users);
-});
+router.post('/', createUser);
 
-router.post(''/'', (req, res) => {
-    console.log('POST ROUTE REACHED');
-    const user = req.body;
-    users.push({ ...user, id: uuidv4() });
-    res.send(`User with the name ${user.firstName} added to the database!`);
-} );
+router.get('/:id', getUser);
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-})
+router.delete('/:id', deleteUser);
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with the id ${id} deleted from the database.`);
-})
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-    const user = users.find((user) => user.id === id);
-    if (firstName) user.firstName = firstName;
-    if (lastName) user.lastName = lastName;
-    if (age) user.age = age;
-    res.send(`User with the id ${id} has been updated`);
-})
+router.patch('/:id', updateUser);
 
 export default router;
