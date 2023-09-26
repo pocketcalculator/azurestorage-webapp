04f766c338c9a97a1e83614af0b2b056731574a4
Paul Sczurek <pocketcalculatorshow@gmail.com>
2023-09-25 22:19:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import userRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {res.send('Hello from Homepage')});
+
+app.listen(PORT, () => console.log(`Server Running on http://localhost:${PORT}`));
